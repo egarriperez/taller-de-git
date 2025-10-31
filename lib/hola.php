7abbd0e0a5ebd8f8df93f91799869eeef0fe6397
e.garriperez <e.garriperez@edu.gva.es>
2025-10-31 19:28:12 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php 
-// Autor: Eduardo Garri <e.garriperez@edu.gva.es> 
-// El nombre por defecto es Mundo 
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo"; 
-@print "Hola, {$nombre}\n"; 
+// Autor: Sergio Gómez <sergio@uco.es> 
+print "Introduce tu nombre:"; 
+$nombre = trim(fgets(STDIN)); 
+@print "Hola, {$nombre}\n";
